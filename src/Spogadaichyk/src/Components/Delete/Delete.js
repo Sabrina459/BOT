dc5f5dbe6ab8322cdf54df71bfed1fa4645a4084
Sabrina459 <62243357+Sabrina459@users.noreply.github.com>
2021-05-25 23:14:27 +0300

    Update Delete.js

--- src/Spogadaichyk/src/Components/Delete/Delete.js
+++ src/Spogadaichyk/src/Components/Delete/Delete.js
@@ -2,8 +2,15 @@ import { Text, useText, useBotContext } from '@urban-bot/core';
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { deleteUser } from '../mongodb/botController';
+import { useRouter } from '@urban-bot/core';
 
+/**
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
 function Delete() {
+    const { navigate } = useRouter();
     const [answer, setAnswer] = useState('Deleting user...');
     const { chat } = useBotContext();
 
@@ -13,15 +20,19 @@ function Delete() {
     const name = chat.firstName
 
     useEffect(()=>{
-        deleteUser(userId).then(()=>setAnswer("Delete successfully"))
+        deleteUser(userId).then(({message})=>{
+            setAnswer(message)
+            navigate('/start')
+        })
+
     },[])
 
     return (
         <Text>
             {answer}
         </Text>
+
     );
 }
-
 
 export default Delete;
